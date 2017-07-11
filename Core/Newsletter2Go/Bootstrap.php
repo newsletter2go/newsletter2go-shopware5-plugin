40957c201c10a70b47b1aae156e31c0b26f1d2cc
MiMo <moritz@newsletter2go.com>
2017-07-11 16:02:30 +0200

    Update to SW_4_1_09

--- Core/Newsletter2Go/Bootstrap.php
+++ Core/Newsletter2Go/Bootstrap.php
@@ -39,7 +39,7 @@ class Shopware_Plugins_Core_Newsletter2Go_Bootstrap extends Shopware_Components_
     public function getVersion()
     {
 
-        return '4.1.07';
+        return '4.1.09';
 
     }
 
