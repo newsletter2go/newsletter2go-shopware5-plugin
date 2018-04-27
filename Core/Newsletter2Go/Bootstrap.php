c6aae8446162506752e4dcb141702a07bdc40a95
Christian Sciberras <christian@sciberras.me>
2018-04-27 09:25:47 +0200

    Increased version

--- Core/Newsletter2Go/Bootstrap.php
+++ Core/Newsletter2Go/Bootstrap.php
@@ -8,7 +8,7 @@ use Newsletter2Go\Components\Newsletter2GoHelper;
  */
 class Shopware_Plugins_Core_Newsletter2Go_Bootstrap extends Shopware_Components_Plugin_Bootstrap
 {
-    const VERSION = '4.1.17';
+    const VERSION = '4.1.18';
 
     /**
      * Capabilities for plugin.
